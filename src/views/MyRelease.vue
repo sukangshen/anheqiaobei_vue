ccb5d34a447f574c59ba06f60deb326bbf9c60de
zhuweibing <zhuweibing@bicai365.com>
2019-11-03 14:43:53 +0800

    11

--- src/views/MyRelease.vue
+++ src/views/MyRelease.vue
@@ -1,28 +1,20 @@
 <template>
   <div class="myRelease">
-    <van-card
-      num="2"
-      price="2.00"
-      desc="描述信息"  
-      title="商品标题"
-      thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
-    />
+    123
   </div>
 </template>
 
 <script>
-import { card } from 'vant';
 import {createOrder} from "@/request/api.js";
 export default {
   name: "myRelease",
   data() {
     return {
-      value:''
     };
   },
   methods: {},
   mounted() {
-      createOrder({openid:sessionStorage.openId,total_fee:'0.1',goods_name:'爱情'}).then(res=>{
+      createOrder({openid:sessionStorage.openId,total_fee:'1',goods_name:'爱情'}).then(res=>{
         alert(JSON.stringify(res));
       })
   }
