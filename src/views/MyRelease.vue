fb6e92a7eb6da5fef5359fc9fe91a7a765a2e8d0
zhuweibing <zhuweibing@bicai365.com>
2019-11-03 15:51:23 +0800

    支付

--- src/views/MyRelease.vue
+++ src/views/MyRelease.vue
@@ -13,18 +13,69 @@ export default {
     return {
     };
   },
-  methods: {},
+  methods: {
+     onBridgeReady(params) {
+            const pay_params = JSON.parse(params);
+            WeixinJSBridge.invoke(
+                'getBrandWCPayRequest', {
+                    "appId": pay_params.appId,  //公众号名称，由商户传入     
+                    "timeStamp": pay_params.timeStamp,  //时间戳，自1970年以来的秒数     
+                    "nonceStr": pay_params.nonceStr,  //随机串     
+                    "package": pay_params.package,     
+                    "signType": pay_params.signType,  //微信签名方式：     
+                    "paySign": pay_params.paySign  //微信签名 
+                },
+                function(res){
+                    if(res.err_msg == "get_brand_wcpay_request:ok" ){
+                    alert('支付成功！');
+                } 
+            }); 
+        },
+        wxpay() {
+            axios.post(url,data)
+            .then((res) => {
+                if(res.code == 200) {
+                    const pay_params = res.data.jsApiParameters
+                    if (typeof WeixinJSBridge == "undefined"){
+                        if( document.addEventListener ){
+                            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                        }else if (document.attachEvent){
+                            document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+                            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                        }
+                        }else{
+                        this.onBridgeReady(pay_params);
+                    }
+                }else{
+                    alert('微信支付调起失败！');
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+        },
+  },
   mounted() {
       createOrder({openid:sessionStorage.openId,total_fee:'1',goods_name:'爱情'}).then(res=>{
         alert(JSON.stringify(res));
-        wx.config({
-                    debug: false,
-                    appId: res.data.appId,
-                    timestamp: res.data.timestamp,
-                    nonceStr: res.data.nonceStr,
-                    signature: res.data.sign,
-                    jsApiList: ["chooseWXPay"]
-                });
+        const pay_params = res.data;
+                    if (typeof WeixinJSBridge == "undefined"){
+                        if( document.addEventListener ){
+                            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                        }else if (document.attachEvent){
+                            document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+                            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                        }
+                        }else{
+                        this.onBridgeReady(pay_params);
+                    }
+              // wx.config({
+              //       debug: false,
+              //       appId: res.data.appId,
+              //       timestamp: res.data.timestamp,
+              //       nonceStr: res.data.nonceStr,
+              //       signature: res.data.sign,
+              //       jsApiList: ["chooseWXPay"]
+              //   });
                 // resolve(res);
       })
   }
