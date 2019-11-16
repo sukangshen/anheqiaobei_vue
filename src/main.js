92e72a2d54b646ea40be11f50cc6a160e9190f1f
kangshen su <kangshensu@gmail.com>
2019-11-16 18:45:17 +0800

    test

--- src/main.js
+++ src/main.js
@@ -18,6 +18,26 @@ new Vue({
 import VConsole from 'vconsole'
 new VConsole()
 
+
+if(sessionStorage.token){
+    // 如果存在什么也不做，直接用token就行
+    alert('缓存'+sessionStorage.token);
+}else if(location.href.includes('token')){
+    alert('返回111'+location.href);
+    alert('返回TOKEN'+this.$route.query.token);
+    alert('返回'+JSON.stringify(this.$route.query));
+    sessionStorage.token = this.$route.query.token;
+    alert('sessionStorage:' + JSON.stringify(sessionStorage));
+}else{
+    var en = escape('http://love.anheqiaobei.com/#/about');
+    // var en = 'http://love.anheqiaobei.com';
+    var url = 'http://api.love.anheqiaobei.com/api/wechat/auth?target_url='+en;
+    alert('请求'+url);
+     window.location.href=url;
+
+}
+
+
 // router.beforeEach((to, from, next) => {
 //   console.log(to);
 //   console.log(from)
